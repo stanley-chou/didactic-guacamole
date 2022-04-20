e9acfa22f87b4c84993e199948bf274208703e82
stanley-chou <68239230+stanley-chou@users.noreply.github.com>
2022-04-19 21:26:28 -0400

    Add files via upload

--- voiceslice.m
+++ voiceslice.m
@@ -2,7 +2,7 @@ function [slice] = voiceslice(v, T, Fs)
 %generates AR coefficients and excites it with impulse based off of pitch.
 
 
-ArCoeff = lpc(v, 30);
+ArCoeff = lpc(v, 100);
 impulseTrain = zeros(1,length(v));
 impulseTrain(1:T:end) = 1;
 slice = filter(1,ArCoeff,impulseTrain);
