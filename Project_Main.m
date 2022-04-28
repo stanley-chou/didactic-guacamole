9e8f43c3604446f3e896b16be7a60435a2684cbd
stanley-chou <68239230+stanley-chou@users.noreply.github.com>
2022-04-27 22:04:30 -0400

    final version

--- Project_Main.m
+++ Project_Main.m
@@ -1,15 +1,13 @@
-%DSP Final Project
+
+%DSP Final Project Code Group 15
 
 %Question 1
 
-disp('initial commit')
-disp('testing')
-
-
+%reading the audio file
 [v,Fs] = audioread('poggers2.wav');
 
 % Question 2
-
+%applying pre-emphasis to the audio file
 v = v(:,1);
 y = v;
 c = 0.98;
@@ -21,37 +19,47 @@ end
 
 %Question 3
 
+%Get the DFt of the Autocorrelation of y(n)
+%y1 = abs(fft(xcorr(y,30)));
 y1 = abs(fft(xcorr(y,y)));
 figure(1)
 stem(y1);
 title('DFT of auto correlation of y(n)');
-xlabel("omega")
+xlabel("\omega")
 ylabel("|H(w)|")
 
 %Question 4
 
+%Get DFT of the magnitude squared of y(n)
 y2 = abs(fft(y)).^2;
 figure(2)
 stem(y2);
 title('DFT of the magnitude squared of y(n)');
-xlabel("omega")
+xlabel("\omega")
 ylabel("|H(w)|")
 
 %Question 5  
 
 
-[a,g] = lpc(y,30); %getting 30 AR coefficients for the funciton
+[a,g] = lpc(y,30); %getting 30 AR coefficients for the the pre-emphasized voice signal
 
 % Question6
 
+figure(70)
+y= y(63024:73187);
+plot(y);
+title("Voiced Segment Example")
+xlabel("sample number")
+ylabel("amplitude")
 ar = filter(1,a,y); % y(n) = voice*ARfilter
+%ar = filter(a,1,v); % y(n) = voice*ARfilter, maybe look at v
 
 ARw = fft(ar, length(v));
 frequencies = linspace(0,Fs,length(v));
 figure(3)
 plot(frequencies, abs(ARw),  '--b');
 hold on
-plot(frequencies, abs(fft(y,length(v))), 'or');
+plot(frequencies, abs(fft(y,length(v))), 'r');
 title('Spectrum of y(n) Overlapped with the AR Specturm)');
 xlabel("omega")
 ylabel("|H(w)|")
@@ -69,6 +77,16 @@ title('Spectrum of y(n)')
 xlabel("omega")
 ylabel("|H(w)|")
 
+figure(69)
+plot(frequencies, abs(ARw),  'b');
+hold on
+plot(frequencies, 7.*abs(fft(y,length(v))), 'r');
+title('Spectrum of y(n) multiplied by 7 Overlapped with the AR Specturm)');
+xlabel("omega")
+ylabel("|H(w)|")
+legend('AR Spectrum', 'y(n) Spectrum');
+hold off
+
 % Question 7
 %formants are equal to the roots of A 
 formants = roots(a);
@@ -76,57 +94,49 @@ formants = roots(a);
 
 %Question 8
 %method 1
-f0 = pitch(y,Fs);
+ %pitch period and frequency via the pitch function
+f00 = pitch(v,Fs);
+pitchPeriod = Fs/(mean(f00)); %might be more accurate to choose the most common occurring value, or the median
 
-[femalev,femaleFs] = audioread('Female_Voice.wav');
+ %primarily the group picked the value by directly looking at the pitch
+ %plot as it was the most accurate
 
-femalev = femalev(:,1);
-femaley = femalev;
-c = 0.98;
-
-for n= 2:length(femalev)
-    femaley(n) = femalev(n) - c*femalev(n-1);
-end
-
-
-femalef0 = pitch(femaley,femaleFs);
-[otherv,otherFs] = audioread('poggers.wav');
-otherv = otherv(:,1);
-othery = otherv;
-c = 0.98;
-
-for n= 2:length(otherv)
-    othery(n) = otherv(n) - c*otherv(n-1);
-end
-otherf0 = pitch(othery,otherFs);
-
-figure()
-stem(femalef0);
-title("pitch of female voice");
-figure()
-stem(f0);
-title("pitch of voice sample");
-figure()
-stem(otherf0);
-title("pitch of other male voice");
 % method 2
+%pitch period and frequency via the rceps function
 
 [cepstrum,recon] = rceps(y);
-figure()
+figure(7)
 
 semilogy(abs(recon));
-title('cepstrum of pre-emphasized voice y(n)')
+title('cepstrum of pre-emphasized voice y(n)Female')
+figure(8)
 
+semilogx(abs(recon));
+title('cepstrum semilogx of pre-emphasized voice y(n)')
 %method1 get 110,250
 %method2 get 111,860
 %Noah's voice gets 149,784
 
+%experiments on female voice
 
+[v,Fs] = audioread('Female_Voice.wav');
 
+y  = preEmphasis(v);
+
+figure(50)
+plot(y)
+y = y(36417:67824);
+fem = pitch(v,Fs);
+[cepstrum,recon] = rceps(y);
+figure(12)
+semilogy(abs(recon));
+%soundsc(y,Fs);
+%pause(2);
+title('cepstrum of pre-emphasized female voice y(n)')
 
 %Question 9
 %classnumber, file number
-
+%intraclass and interclass distance measures
 % class 1
 
 NumSigPerClass = 10;
@@ -146,7 +156,6 @@ v1{i} = preEmphasis(v1{i});
     class2(i,:) = v2{i};
     interclass(i,:) = v1{i};
     interclass(NumSigPerClass+i,:) = v2{i};
-
 end
 i=NumSigPerClass+1;
 hi = strcat('poggers.wav');
@@ -174,12 +183,17 @@ cosintraClass2 = cosDistance(class2)
 cosinterClassDistance = cosDistance(interclass)
 
 %Question 10
+
+% Voice recreation
+
 %Noah Voice
 [v,Fs] = audioread('poggers.wav');
+
 y  = preEmphasis(v);
-y = y(27297:57187);
-% figure(10);
-% plot(y);
+figure(10);
+plot(y);
+y = y(4701:57553);
+
 numberof = (length(y)/380);
 splits = floor(numberof);
 recreated = recreater(y,Fs, splits, 380);
@@ -198,8 +212,11 @@ y  = preEmphasis(v);
 f02 = pitch(y,Fs);
 pitchPeriod2 = floor(Fs/(mode(f02)));
 y = y(4241:30425);
-% figure(11);
-% plot(y);
+figure(11);
+plot(y);
+title("Stanley Poggers")
+xlabel("sample number")
+ylabel("amplitude")
 numberof = (length(y)/373);
 splits = ceil(numberof);
 recreated = recreater(y,Fs, splits, 373);
@@ -210,14 +227,20 @@ pause(2);
 recreatedFilt = lowpass(recreated,8000,Fs);
 soundsc(recreatedFilt,Fs);
 
+
+% not really worked on, but quick female voice recreation. 
+% [v,Fs] = audioread('Female_Voice.wav');
+% y  = preEmphasis(v);
+% y = y(36417:67824);
+% numberof = (length(y)/173);
+% splits = ceil(numberof);
+% recreated = recreater(y,Fs, splits, 173);
+% soundsc(y,Fs);
+% pause(2);
+% soundsc(recreated,Fs);
+% pause(2);
+% recreatedFilt = lowpass(recreated,8000,Fs);
+% soundsc(recreatedFilt,Fs);
 %number how many segments do you want to break the signal into x segments
 %of equal amount samples
 
-%for each individual segment take an independent AR coefficient set, take the LPC
-
-%Then create an impulse train from 1 spaced equally btwn pitch period
-% impulse = 
-
-%take each LPC segment and their a and convolve with the impulse function
-% then put them all together
-% and play them.
